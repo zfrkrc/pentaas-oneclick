38829eaa9ba48ac3611f89504472d590cddebf84
zfrkrc <zfrkrc@gmail.com>
2026-02-22 06:00:03 +0300

    feat: Update Navbar to replace Posta Hizmeti link with Posta Hizmetleri for improved navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -55,7 +55,6 @@ const Navbar = () => {
 
                     <ul className="pnav-links">
                         <li><a className="nav-link link text-black show text-primary display-4" href="https://zaferkaraca.net/#testimonials-8-uSrJKo6yl4" aria-expanded="false">Hakkımızda</a></li>
-                        <li><a className="nav-link link text-black show text-primary display-4" href="https://postaci.zaferkaraca.net">Posta Hizmeti</a></li>
                         <li><a className="nav-link link text-black show text-primary display-4" href="https://zaferkaraca.net/posta.html">Posta Hizmetleri</a></li>
                         <li><a className="nav-link link text-black show text-primary display-4" href="https://hobby.zaferkaraca.net">Hobby</a></li>
                         <li className={`pnav-dd ${isOpen ? 'open' : ''}`}>
@@ -81,7 +80,6 @@ const Navbar = () => {
                 {isOpen && (
                     <div className="pnav-mobile">
                         <a href="https://zaferkaraca.net/#testimonials-8-uSrJKo6yl4">Hakkımızda</a>
-                        <a href="https://postaci.zaferkaraca.net">Posta Hizmeti</a>
                         <a href="https://zaferkaraca.net/posta.html">Posta Hizmetleri</a>
                         <a href="https://hobby.zaferkaraca.net">Hobby</a>
                         <a href="https://pentestone.zaferkaraca.net/">PentestOne</a>
