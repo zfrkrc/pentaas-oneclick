b25b30633f4af1762bda405b0f72bd4572c8c974
zfrkrc <zfrkrc@gmail.com>
2026-02-22 07:14:59 +0300

    feat: Remove unused configuration and script files for cleanup

--- .temp_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to update all Dockerfile COPY paths for new build context
-
-# Simple services (arjun, dirsearch, dnsrecon, sslyze, wafw00f)
-for service in arjun dirsearch dnsrecon sslyze wafw00f whatweb; do
-  echo "Updating $service..."
-done
